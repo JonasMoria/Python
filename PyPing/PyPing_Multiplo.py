e34fdddc030d75561312c6a19dc1cc01dbe84d49
Jonas Moria <moriajonas@gmail.com>
2022-04-21 13:08:15 -0300

    Aplicadas Algumas Correções

--- PyPing/PyPing_Multiplo.py
+++ PyPing/PyPing_Multiplo.py
@@ -4,8 +4,9 @@ import os
 print('\n=== PY PING ===\n')
 
 targets = []
-#Limpando o arquivo txt para novas entradas de dados
-with open('LOGS\\LOGS.txt', 'r+') as file:
+
+# Limpando o arquivo txt para novas entradas de dados
+with open('LOGS.txt', 'r+') as file:
     file.truncate()
 
 # Caminho do arquivo:
@@ -22,9 +23,9 @@ for hosts in targets:
     print("Analisando..."+hosts)
 
     # Executando o ping
-    os.system('ping -n 4 {} >> LOGS\\LOGS.txt'.format(hosts))
+    os.system('ping -n 4 {} >> LOGS.txt'.format(hosts))
     os.system(
-        'echo ============================================================== >> LOGS\\LOGS.txt')
+        'echo ============================================================== >> LOGS.txt')
 
 
 print("\n")
